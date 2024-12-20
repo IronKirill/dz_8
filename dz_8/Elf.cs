90c0b47f722b6a48f466372cdf2b70dc1e57aeb9
kirillboychuk <kiryllboychuk@gmail.com>
2024-12-20 18:27:22 +0200

    Add project files.

---
+++ dz_8/Elf.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz_8
+{
+    public abstract class ElvenFighter
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Karma { get; set; }
+        public int Magic { get; set; }
+        public string Weapon { get; set; }
+
+        protected ElvenFighter(string name, int health, int karma, int magic, string weapon)
+        {
+            Name = name;
+            Health = health;
+            Karma = karma;
+            Magic = magic;
+            Weapon = weapon;
+        }
+
+        public abstract void Attack();
+        public abstract void MakeSound();
+        public void showStatus()
+        {
+            Console.WriteLine($"Имя: {Name}, Здоровье: " +
+                $"{Health}, Карма: {Karma}, Магия: {Magic}, Оружие: {Weapon}");
+        }
+    }
+
+
+    public class ElvenKnight : ElvenFighter
+    {
+        public ElvenKnight(string name, int health, int karma, int magic, string weapon)
+            : base(name, health, karma, magic, weapon) { }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"{Name} атакует своим оружием '{Weapon}' с яростью!");
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} кричит: 'За честь и славу!'");
+        }
+    }
+
+
+    public class ElvenScout : ElvenFighter
+    {
+        public ElvenScout(string name, int health, int karma, int magic, string weapon)
+            : base(name, health, karma, magic, weapon) { }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"{Name} стреляет из оружия '{Weapon}' с точностью!");
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} говорит: 'Моя стрела не промахнется!'");
+        }
+    }
+
+
+    public class TempleKnight : ElvenKnight
+    {
+        public TempleKnight(string name, int health, int karma, int magic, string weapon)
+            : base(name, health, karma, magic, weapon) { }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"{Name} атакует своим оружием '{Weapon}' с яростью!");
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} кричит: 'За честь и славу!'");
+        }
+    }
+
+
+    public class SwordSinger : ElvenKnight
+    {
+        public SwordSinger(string name, int health, int karma, int magic, string weapon)
+            : base(name, health, karma, magic, weapon) { }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"{Name} атакует своим оружием '{Weapon}' с яростью!");
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} кричит: 'За честь и славу!'");
+        }
+    }
+
+
+    public class PlainsWalker : ElvenScout
+    {
+        public PlainsWalker(string name, int health, int karma, int magic, string weapon)
+            : base(name, health, karma, magic, weapon) { }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"{Name} использует магию и оружие '{Weapon}' для атаки!");
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} произносит заклинание: 'Arcanum Explosio!'");
+        }
+    }
+
+
+    public class SilverRanger : ElvenScout
+    {
+        public SilverRanger(string name, int health, int karma, int magic, string weapon)
+            : base(name, health, karma, magic, weapon) { }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"{Name} стреляет из оружия '{Weapon}' с точностью!"); ;
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} говорит: 'Моя стрела не промахнется!'");
+        }
+    }
+
+
+    public class GuasTemplar : TempleKnight
+    {
+        public GuasTemplar(string name, int health, int karma, int magic, string weapon)
+            : base(name, health, karma, magic, weapon) { }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"{Name} использует магию и оружие '{Weapon}' для атаки!");
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} произносит заклинание: 'Arcanum Explosio!'");
+        }
+    }
+
+
+    public class SwordMuse : SwordSinger
+    {
+        public SwordMuse(string name, int health, int karma, int magic, string weapon)
+            : base(name, health, karma, magic, weapon) { }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"{Name} атакует своим оружием '{Weapon}' с яростью!");
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} кричит: 'За честь и славу!'");
+        }
+    }
+
+
+    public class WindRider : PlainsWalker
+    {
+        public WindRider(string name, int health, int karma, int magic, string weapon)
+            : base(name, health, karma, magic, weapon) { }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"{Name} стреляет из оружия '{Weapon}' с точностью!");
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} говорит: 'Моя стрела не промахнется!'");
+        }
+    }
+
+
+    public class MoonlightSentimel : SilverRanger
+    {
+        public MoonlightSentimel(string name, int health, int karma, int magic, string weapon)
+            : base(name, health, karma, magic, weapon) { }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"{Name} использует магию и оружие '{Weapon}' для атаки!");
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} произносит заклинание: 'Arcanum Explosio!'");
+        }
+    }
+}
---
+++ dz_8/Program.cs
@@ -0,0 +1,105 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Threading;
+using System;
+
+namespace dz_8
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("______________________________");
+            Console.WriteLine("| ВЫБЕРИТЕ ГЕРОЯ:            |");
+            Console.WriteLine("|                            |");
+            Console.WriteLine("| 1. ElvenKnight             |");
+            Console.WriteLine("| 2. ElvenScout              |");
+            Console.WriteLine("| 3. TempleKnight            |");
+            Console.WriteLine("| 4. SwordSinger             |");
+            Console.WriteLine("| 5. PlainsWalker            |");
+            Console.WriteLine("| 6. SilverRanger            |");
+            Console.WriteLine("| 7. GuasTemplar             |");
+            Console.WriteLine("| 8. SwordMuse               |");
+            Console.WriteLine("| 9. WindRider               |");
+            Console.WriteLine("| 10. MoonlightSentimel      |");
+            Console.WriteLine("|____________________________|");
+
+            int choice = int.Parse(Console.ReadLine() ?? "1");
+
+            ElvenFighter heroes = choice switch
+            {
+                1 => new ElvenKnight("Elven Knight", 100, 50, 20, "Меч"),
+                2 => new ElvenScout("Elven Scout", 80, 60, 40, "Лук"),
+                3 => new TempleKnight("Temple Knight", 100, 50, 20, "Меч"),
+                4 => new SwordSinger("Sword Singer", 100, 50, 20, "Меч"),
+                5 => new PlainsWalker("Plains Walker", 70, 30, 100, "Копьё"),
+                6 => new SilverRanger("Silver Ranger", 80, 60, 40, "Арбалет"),
+                7 => new GuasTemplar("Guas Templar", 70, 30, 100, "Топор"),
+                8 => new SwordMuse("Sword Muse", 100, 50, 20, "Меч"),
+                9 => new WindRider("Wind Rider", 80, 60, 40, "Арбалет"),
+                10 => new MoonlightSentimel("Moonlight Sentimel", 70, 30, 100, "Копьё"),
+                _ => new ElvenKnight("Elven Knight", 100, 50, 20, "Меч")
+            };
+
+            Console.WriteLine($"ВЫ ВЫБРАЛИ ГЕРОЯ: {heroes.Name}");
+            heroes.showStatus();
+
+            ElvenFighter enemy = new TempleKnight("Temple Knight ENEMY", 200, 100, 40, "Меч");
+            Console.WriteLine($"\nВАШ ПРОТИВНИК: {enemy.Name}");
+            enemy.showStatus();
+
+            Console.WriteLine("\nБИТВА НАЧИНАЕТСЯ!");
+
+            while (heroes.Health > 0 && enemy.Health > 0)
+            {
+                Console.WriteLine("________________________________");
+                Console.WriteLine("| ВЫБЕРИТЕ ДЕЙСТВИЕ:           |");
+                Console.WriteLine("|                              |");
+                Console.WriteLine("| 1. АТАКОВАТЬ                 |");
+                Console.WriteLine("| 2. ПОДАТЬ ГОЛОС              |");
+                Console.WriteLine("| 3. ПОКАЗАТЬ СТАТУС           |");
+                Console.WriteLine("|______________________________|");
+
+                int action = int.Parse(Console.ReadLine() ?? "1");
+
+                if (action == 1)
+                {
+                    heroes.Attack();
+                    enemy.Health -= 100;
+
+                    if (enemy.Health <= 0)
+                    {
+                        Console.WriteLine($"{enemy.Name} ПОБЕЖДЁН!");
+                        break;
+                    }
+
+                    enemy.Attack();
+                    heroes.Health -= 10;
+
+                    if (heroes.Health <= 0)
+                    {
+                        Console.WriteLine("ВЫ - ПРОИГРАЛИ!");
+                        break;
+                    }
+                }
+
+                else if (action == 2)
+                {
+                    heroes.MakeSound();
+                }
+
+                else if (action == 3)
+                {
+                    heroes.showStatus();
+                    enemy.showStatus();
+                }
+
+                else
+                {
+                    Console.WriteLine("ERROR!");
+                }
+            }
+            Console.WriteLine("\nКОНЕЦ ИГРЫ!");
+            Console.WriteLine("\nВЫ - ВЫИГРАЛИ!");
+        }
+    }
+}
